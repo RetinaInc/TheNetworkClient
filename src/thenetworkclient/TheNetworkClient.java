fbe13835b70b1833a586f4878e4fef13a59d7d0a
Frank Steiler <frank@steiler.eu>
2014-06-21 09:56:47 -0700

    Updated contact information.

--- src/thenetworkclient/TheNetworkClient.java
+++ src/thenetworkclient/TheNetworkClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Frank Steiler <frank@steiler.eu>
+ * Copyright (C) 2014 Frank Steiler <frank.steiler@steilerdev.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ import javafx.stage.StageStyle;
 
 /**
  * This is the entry class for the "The Network" desktop application.
- * @author Frank Steiler <frank@steiler.eu>
+ * @author Frank Steiler <frank.steiler@steilerdev.de>
  */
 public class TheNetworkClient extends Application 
 {
--- src/thenetworkclient/XMLPost.java
+++ src/thenetworkclient/XMLPost.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Frank Steiler <frank@steiler.eu>
+ * Copyright (C) 2014 Frank Steiler <frank.steiler@steilerdev.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@ import java.text.SimpleDateFormat;
 
 /**
  * This class represents a post.
- * @author Frank Steiler <frank@steiler.eu>
+ * @author Frank Steiler <frank.steiler@steilerdev.de>
  */
 public class XMLPost 
 {
